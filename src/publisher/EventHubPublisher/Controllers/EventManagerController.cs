0126d2e98e279d8263fdd6dea134ebeb9abe3873
Dan Maxim <maxdanus@yahoo.com>
2019-02-22 11:56:03 -0500

    create project shell

---
+++ src/publisher/EventHubPublisher/Controllers/EventManagerController.cs
@@ -0,0 +1,27 @@
+﻿using EventHubPublisher.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EventHubPublisher.Controllers
+{
+    public class EventManagerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+	        return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Create(NewEventModel newEventModel)
+        {
+	        return RedirectToAction("Index");
+        }
+    }
+}
---
+++ src/publisher/EventHubPublisher/Models/NewEventModel.cs
@@ -0,0 +1,8 @@
+﻿
+namespace EventHubPublisher.Models
+{
+	public class NewEventModel
+	{
+		public string Message { get; set; }
+	}
+}
