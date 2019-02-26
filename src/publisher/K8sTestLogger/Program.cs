edc020ba1ee624926cea46217c9032630e62e6bd
Dan Maxim <maxdanus@yahoo.com>
2019-02-26 14:46:31 -0500

    try to add elastic search formatter

--- src/publisher/K8sTestLogger/Program.cs
+++ src/publisher/K8sTestLogger/Program.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.IO;
+using System.Runtime.InteropServices.ComTypes;
 using System.Threading;
+using Microsoft.AspNetCore.Razor.Language;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging.Configuration;
 using Serilog;
 
 namespace K8sTestLogger
@@ -10,6 +14,8 @@ namespace K8sTestLogger
 	{
 		public static void Main(string[] args)
 		{
+
+
 			var builder = new ConfigurationBuilder()
 				.SetBasePath(Directory.GetCurrentDirectory())
 				.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
@@ -28,10 +34,29 @@ namespace K8sTestLogger
 			{
 				Thread.Sleep(1000);
 
-				logger.Warning($"Logging a message with counter {i}");
+				logger.Warning($"Logging from the K8s logger a message with counter {i}");
 			}
 
 			Thread.Sleep(5000);
+		}
+
+
+		private static void StartUp()
+		{
+			var builder = new HostBuilder()
+				.ConfigureAppConfiguration((hostingContext, config) =>
+					{
+						config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+						config.AddEnvironmentVariables();
+					})
+				.ConfigureLogging((hostingContext, logging) =>
+					{
+						logging.AddConfiguration();
+						logging.AddSerilog();
+					}
+				);
+
+
 		}
 	}
 }
