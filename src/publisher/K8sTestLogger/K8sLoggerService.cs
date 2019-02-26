9c41e98700e316117c4675acb6631f15fa0b6d05
Dan Maxim <maxdanus@yahoo.com>
2019-02-26 15:50:11 -0500

    remove aspnet core reference

--- src/publisher/K8sTestLogger/K8sLoggerService.cs
+++ src/publisher/K8sTestLogger/K8sLoggerService.cs
@@ -1,37 +1,37 @@
-﻿using System.Threading;
-using System.Threading.Tasks;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+﻿//using System.Threading;
+//using System.Threading.Tasks;
+//using Microsoft.Extensions.Hosting;
+//using Microsoft.Extensions.Logging;
 
-namespace K8sTestLogger
-{
-	public class K8sLoggerService : IHostedService
-	{
-		private readonly ILogger<K8sLoggerService> _logger;
-		public K8sLoggerService(ILogger<K8sLoggerService> logger)
-		{
-			_logger = logger;
-		}
+//namespace K8sTestLogger
+//{
+//	public class K8sLoggerService : IHostedService
+//	{
+//		private readonly ILogger<K8sLoggerService> _logger;
+//		public K8sLoggerService(ILogger<K8sLoggerService> logger)
+//		{
+//			_logger = logger;
+//		}
 
-		public Task StartAsync(CancellationToken cancellationToken)
-		{
-			var logDetail = new LogDetail("Test User", "Test action", "First log entry");
-			_logger.LogWarning("Test Application Starting {@logDetail} ", logDetail);
+//		public Task StartAsync(CancellationToken cancellationToken)
+//		{
+//			var logDetail = new LogDetail("Test User", "Test action", "First log entry");
+//			_logger.LogWarning("Test Application Starting {@logDetail} ", logDetail);
 
 
-			for (var i = 0; i < 1000; i++)
-			{
-				Thread.Sleep(1000);
-				logDetail.Message = $"Logging with index {i}";
-				_logger.LogWarning($"Logging from the K8s logger a message with counter {i} {@logDetail}", logDetail);
-			}
+//			for (var i = 0; i < 1000; i++)
+//			{
+//				Thread.Sleep(1000);
+//				logDetail.Message = $"Logging with index {i}";
+//				_logger.LogWarning($"Logging from the K8s logger a message with counter {i} {@logDetail}", logDetail);
+//			}
 
-			return Task.CompletedTask;
-		}
+//			return Task.CompletedTask;
+//		}
 
-		public Task StopAsync(CancellationToken cancellationToken)
-		{
-			return Task.CompletedTask;
-		}
-	}
-}
+//		public Task StopAsync(CancellationToken cancellationToken)
+//		{
+//			return Task.CompletedTask;
+//		}
+//	}
+//}
--- src/publisher/K8sTestLogger/Program.cs
+++ src/publisher/K8sTestLogger/Program.cs
@@ -4,7 +4,6 @@ using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using Serilog;
 using Serilog.Enrichers.UsafStandard;
 
